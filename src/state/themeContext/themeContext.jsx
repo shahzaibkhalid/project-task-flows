04743c04e61fcd9f093c4c7a58ae0067e54ea044
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-06-08 23:59:05 -0400

    feat(theme): :lipstick: add `theme` object for `theme-ui`

---
+++ src/state/themeContext/themeContext.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { createContext, useContext, useMemo, useState } from 'react';
+
+const themeValues = {
+  dark: 'dark',
+  light: 'light',
+};
+
+const ThemeContext = createContext();
+
+function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error(
+      'Error: useTheme custom hook must be used within ThemeContext!'
+    );
+  }
+
+  return context;
+}
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState(themeValues.dark);
+
+  const value = useMemo(() => [theme, setTheme], [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
+
+ThemeProvider.defaultProps = {
+  children: null,
+};
+
+ThemeProvider.propTypes = {
+  children: PropTypes.element,
+};
+
+export { themeValues, useTheme, ThemeProvider };
