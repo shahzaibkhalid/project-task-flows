61e1b947041d6b43a66c738a73ed99520e9d82f8
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-06-15 17:27:23 -0400

    chore(theme): :lipstick: add `emotion` global

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,14 @@
+import { Global } from '@emotion/react';
 import { StrictMode } from 'react';
 import GenericErrorBoundary from 'containers/GenericErrorBoundary';
 import Providers from 'containers/Providers';
 import Routes from 'containers/Routes';
+import globalStyles from 'theme/globalStyles';
 
 function App() {
   return (
     <StrictMode>
+      <Global styles={globalStyles} />
       <GenericErrorBoundary>
         <Providers>
           <Routes />
---
+++ src/theme/globalStyles.js
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react';
+
+const globalStyles = css({
+  body: {
+    color: 'red',
+  },
+});
+
+export default globalStyles;
