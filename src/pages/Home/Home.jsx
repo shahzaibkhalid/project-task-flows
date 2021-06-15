5b5a34000e7800630f159c093ccc32fee09bf7de
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-06-15 17:18:43 -0400

    perf(theme): :zap: move to scoped `@theme-ui/*` from `theme-ui` to improve perf

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,7 @@
+import { Heading } from '@theme-ui/components';
 import { memo } from 'react';
-import { Heading } from 'theme-ui';
 
 function Home() {
-  // return <h1 sx={{ color: 'primary', p: 1 }}>This is Home Page.</h1>;
   return (
     <Heading as='h1' p={2}>
       This is Home Page.
