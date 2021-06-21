9d19b7a7a51aa1e211892b04db3d7e4322f7223b
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-06-20 22:22:44 -0400

    chore(utils): :fire: add

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+const NODE_TYPES = {
+  task: 'taskNode',
+};
+
+const EDGE_TYPES = {};
+
+const ELEMENT_NAMES = {
+  edge: 'flow-edge',
+  node: 'flow-node',
+};
+
+export { NODE_TYPES, EDGE_TYPES, ELEMENT_NAMES };
