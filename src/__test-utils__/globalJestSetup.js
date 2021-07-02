66cf88ddc2859ac97b4a3f7415dbbca66abe2b57
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-07-01 22:43:09 -0400

    docs(test): :memo: update docs for `globalJestSetup`

--- src/__test-utils__/globalJestSetup.js
+++ src/__test-utils__/globalJestSetup.js
@@ -1,5 +1,8 @@
 require('regenerator-runtime/runtime');
 
+/**
+ * This function will be executed before the very first test suite execution
+ */
 async function globalJestSetup() {}
 
 module.exports = globalJestSetup;
