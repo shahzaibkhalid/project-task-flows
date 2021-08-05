0a2454cdfa6ecbceb4668455eb30f7e7e9673769
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2021-08-05 17:30:17 -0400

    chore: :heavy_minus_sign: remove `react-modal` instances

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 import App from 'containers/App';
 
 const container = document.getElementById('app');
-Modal.setAppElement(container);
 
 ReactDOM.render(<App />, container);
